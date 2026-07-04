38fe8d3e81529279d0eeb82ccd3ef6fd9dcf8c42
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-04 22:58:39 +0600

    Update antiout.js

--- Script/events/antiout.js
+++ Script/events/antiout.js
@@ -1,27 +1,30 @@
 module.exports.config = {
- name: "antiout",
- eventType: ["log:unsubscribe"],
- version: "0.0.1",
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
- description: "Listen events"
+  name: "antiout",
+  eventType: ["log:unsubscribe"],
+  version: "0.0.1",
+  credits: "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+  description: "Listen events"
 };
 
 module.exports.run = async({ event, api, Threads, Users }) => {
- let data = (await Threads.getData(event.threadID)).data || {};
- if (data.antiout == false) return;
- if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
- const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "Koi Ase Pichware Mai Lath Marta Hai?";
- if (type == "self-separation") {
-  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
-   if (error) {
-    api.sendMessage(`সরি বস, ${name} কে আবার এড করতে পারলাম না। 
-সম্ভবত উনি বটকে ব্লক করেছে অথবা তার প্রাইভেসি সেটিংসের কারণে এড করা যায় না। 
-\n──────꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID)
-   } else api.sendMessage(`শোন, ${name}, এই গ্রুপ হইলো গ্যাং!
-এখান থেকে যাইতে হলে এডমিনের পারমিশন লাগে!
-তুই পারমিশন ছাড়া লিভ নিছোস – তোকে আবার মাফিয়া স্টাইলে এড দিলাম।
-\n──────꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID);
-  })
- }
-}
+  let data = (await Threads.getData(event.threadID)).data || {};
+  if (data.antiout == false) return;
+  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "kick";
+  
+  if (type == "self-separation") {
+    api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+      if (error) {
+        
+        const errorMsg = `───────────────\n\n» ⚠️ সরি বস, ${name} কে আবার এড করতে পারলাম না। সম্ভবত উনি বটকে 🫣 করেছে অথবা তার প্রাইভেসি সেটিংসের কারণে এড করা যায় না।\n\n───────────────\n» ─꯭─⃝‌‌🧚𝗦𝗶𝘆𝗮𝗺 𝗖𝗵𝗮𝘁 𝗕𝗼𝘁─⃝‌‌🧚`;
+        api.sendMessage(errorMsg, event.threadID);
+      } else {
+        
+        const successMsg = `───────────────\n\n» 🤫 শোন ${name}, এই গ্রুপ হইলো গ্যাং! এখান থেকে যাইতে হলে এডমিনের পারমিশন লাগে! তুই পারমিশন ছাড়া লিভ নিছোস – তোকে আবার মাফিয়া স্টাইলে এড দিলাম।\n\n───────────────\n» ─꯭─⃝‌‌🧚𝗦𝗶𝘆𝗮𝗺 𝗖𝗵𝗮𝘁 𝗕𝗼𝘁─⃝‌‌🧚`;
+        api.sendMessage(successMsg, event.threadID);
+      }
+    });
+  }
+};
