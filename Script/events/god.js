76f49c57176b9f3eabe5c4f18d568845c9fb3569
mdsiyam01320-tech <mdsiyam01320@gmail.com>
2026-07-04 22:51:14 +0600

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -1,64 +1,66 @@
 module.exports.config = {
-	name: "god",
-	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
-	version: "1.0.0",
-	credits: "SHAHADAT SAHU",
-	description: "Record bot activity notifications!",
-	envConfig: {
-		enable: true
-	}
+  name: "god",
+  eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
+  version: "1.0.0",
+  credits: "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍",
+  description: "Record bot activity notifications!",
+  envConfig: {
+    enable: true
+  }
 };
 
 module.exports.run = async function({ api, event, Threads }) {
-	const logger = require("../../utils/log");
-	if (!global.configModule[this.config.name].enable) return;
-	
-	let formReport = "=== ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
-					"\n\n» Thread ID: " + event.threadID +
-					"\n» Action: {task}" +
-					"\n» Action created by userID: " + event.author +
-					"\n» " + Date.now() + " «";
-	
-	let task = "";
-	
-	switch (event.logMessageType) {
-		case "log:thread-name": {
-			const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist";
-			const newName = event.logMessageData.name || "Name does not exist";
-			task = "User changed group name from: '" + oldName + "' to '" + newName + "'";
-			await Threads.setData(event.threadID, { name: newName });
-			break;
-		}
-		case "log:subscribe": {
-			if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-				task = "The user added the bot to a new group!";
-			}
-			break;
-		}
-		case "log:unsubscribe": {
-			if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) {
-				task = "The user kicked the bot out of the group!";
-			}
-			break;
-		}
-		default: 
-			break;
-	}
+  const logger = require("../../utils/log");
+  if (!global.configModule[this.config.name].enable) return;
+  
+  let task = "";
+  
+  switch (event.logMessageType) {
+    case "log:thread-name": {
+      const oldName = (await Threads.getData(event.threadID)).name || "𝗡𝗮𝗺𝗲 𝗱𝗼𝗲𝘀 𝗻𝗼𝘁 𝗲𝘅𝗶𝘀𝘁";
+      const newName = event.logMessageData.name || "𝗡𝗮𝗺𝗲 𝗱𝗼𝗲𝘀 𝗻𝗼𝘁 𝗲𝘅𝗶𝘀𝘁";
+      task = `𝗨𝘀𝗲𝗿 𝗰𝗵𝗮𝗻𝗴𝗲𝗱 𝗴𝗿𝗼𝘂𝗽 𝗻𝗮𝗺𝗲 𝗳𝗿𝗼𝗺: '${oldName}' 𝘁𝗼 '${newName}'`;
+      await Threads.setData(event.threadID, { name: newName });
+      break;
+    }
+    case "log:subscribe": {
+      if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+        task = "𝗧𝗵𝗲 𝘂𝘀𝗲𝗿 𝗮𝗱𝗱𝗲𝗱 𝘁𝗵𝗲 𝗯𝗼𝘁 𝘁𝗼 𝗮 𝗻𝗲𝘄 𝗴𝗿𝗼𝘂𝗽!";
+      }
+      break;
+    }
+    case "log:unsubscribe": {
+      if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) {
+        task = "𝗧𝗵𝗲 𝘂𝘀𝗲𝗿 𝗸𝗶𝗰𝗸𝗲𝗱 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗼𝘂𝘁 𝗼𝗳 𝘁𝗵𝗲 𝗴𝗿𝗼𝘂𝗽!";
+      }
+      break;
+    }
+    default: 
+      break;
+  }
 
-	if (task.length === 0) return;
+  if (task.length === 0) return;
 
-	formReport = formReport.replace(/\{task}/g, task);
+  const timeString = new Date().toLocaleString("bn-BD", { timeZone: "Asia/Dhaka" });
 
-	const receivers = [
-		"100001039692046",   // Replace youR UID
-		"2056569868083458"   //  Replace youR Group UID
-	];
 
-	for (const id of receivers) {
-		try {
-			await api.sendMessage(formReport, id);
-		} catch (error) {
-			logger(formReport, "[ Logging Event ]");
-		}
-	}
+  const formReport = `───────────────\n\n` +
+                     `» 📢 𝗕𝗼𝘁 𝗡𝗼𝘁𝗶𝗳𝗶𝗰𝗮𝘁𝗶𝗼𝗻\n\n` +
+                     `» 𝗧𝗵𝗿𝗲𝗮𝗱 𝗜𝗗: ${event.threadID}\n` +
+                     `» 𝗔𝗰𝘁𝗶𝗼𝗻: ${task}\n` +
+                     `» 𝗔𝘂𝘁𝗵𝗼𝗿 𝗜𝗗: ${event.author}\n` +
+                     `» 𝗧𝗶𝗺𝗲: ${timeString}\n\n` +
+                     `───────────────\n` +
+                     `» 👤 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍`;
+
+  // config.json থেকে মেইন অ্যাডমিনদের আইডি ডায়নামিকালি নেওয়ার মেকানিজম
+  const receivers = global.config.ADMINBOT || [];
+
+  for (const id of receivers) {
+    try {
+      await api.sendMessage(formReport, id);
+    } catch (error) {
+      logger(formReport, "[ Logging Event ]");
+    }
+  }
 };
